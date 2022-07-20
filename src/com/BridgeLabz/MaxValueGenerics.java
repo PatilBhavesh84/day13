951804b1112b03dd3aa61dba5ec4077001c5d14c
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-20 19:49:07 +0530

    Added:find maximum value of integer

--- src/com/BridgeLabz/MaxValueGenerics.java
+++ src/com/BridgeLabz/MaxValueGenerics.java
@@ -1,9 +1,26 @@
 package com.BridgeLabz;
 
 public class MaxValueGenerics {
+	public static void MaxValue(int a,int b,int c){
+		Integer n1=a;
+		Integer n2=b;
+		Integer n3=c;
+		Integer max=n1;
+		if(n2.compareTo(max)>0) {
 
+			max=n2;
+		}
+		if(n3.compareTo(max)>0) {
+			max=n3;
+		}
+		System.out.println("Maximum number is "+max);
+	}
 	public static void main(String[] args) {
-		System.out.println("Test maximum of 3 variables");
+		System.out.println("TC 1.1 Keeping max value at 1");
+		MaxValue(3,2,1);
+		System.out.println("TC 1.2 keeping max value at 2");
+		MaxValue(2,3,1);
+		System.out.println("TC 1.3 keeping max value at 3");
+		MaxValue(1,2,3);
 	}  
 }  
-
