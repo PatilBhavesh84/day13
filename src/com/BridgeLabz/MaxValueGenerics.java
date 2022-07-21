2763b844501ecd8b7f0c9c8a98d5437274d3dbdf
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-21 16:17:12 +0530

    Modified:For more than 3 parameters

--- src/com/BridgeLabz/MaxValueGenerics.java
+++ src/com/BridgeLabz/MaxValueGenerics.java
@@ -1,25 +1,23 @@
 package com.BridgeLabz;
 
-public class MaxValueGenerics <T extends Comparable<T>>{
-
-	T a1,a2,a3;
-
-	public MaxValueGenerics(T a1,T a2,T a3) {
-		this.a1=a1;
-		this.a2=a2;
-		this.a3=a3;
-	}
-	public static <T extends Comparable<T>> T MaxValue(T a1,T a2,T a3) {
-		T max =a1;
+public class MaxValueGenerics {
+	public static <T extends Comparable<T>>T  MaxValue(T a1,T a2,T a3,T a4,T a5){
+		T max=a1;
 		if(a2.compareTo(max)>0) 
 			max=a2;
 		if(a3.compareTo(max)>0) 
 			max=a3;
+		if(a4.compareTo(max)>0) 
+			max=a4;
+		if(a5.compareTo(max)>0) 
+			max=a5;
 		return max;
 	}
+
+
 	public static void main(String[] args) {
-		System.out.println("Max value in 3 Integers is:"+MaxValue(3,2,1));
-		System.out.println("Max value is 3 Float is:"+MaxValue(3.10f,2.20f,4.10f));
-		System.out.println("Max value is 3 String is:"+MaxValue("Apple","Peach","Banana"));
+		System.out.println("Max value in 3 Integers is:"+MaxValue(3,2,1,4,5));
+		System.out.println("Max value is 3 Float is:"+MaxValue(3.10f,2.20f,1.10f,4.10f,5.20f));
+		System.out.println("Max value is 3 String is:"+MaxValue("Apple","Peach","Banana","Orange","Kivi"));
 	}  
 }
